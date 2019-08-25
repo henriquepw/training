d2f34df6b9e214e94d87f665342ec61ba51a282c
Henrique Miranda <enrque.ns@gmail.com>
2019-08-24 23:12:23 -0300

    chore: new URI problems

---
+++ URI/python/1165.py
@@ -0,0 +1,19 @@
+import math
+
+n = int(input())
+
+for _ in range(n):
+  x = int(input())
+  raiz = int(math.sqrt(x))
+
+  primo = True
+  if (x % 2 != 0):
+    for i in range(3, raiz + 1, 2):
+      if(x % i == 0):
+        primo = False
+        break
+  elif(x != 2):
+    primo = False
+  
+  print(x, 'eh primo' if primo else 'nao eh primo')
+
