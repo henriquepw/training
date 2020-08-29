9160c36d5334e0269556dd8360d2e5cf236752af
Henrique Miranda <enrque.ns@gmail.com>
2020-08-29 10:03:17 -0300

    :sparkles: feat(URI): add question 1157

---
+++ URI/golang/1157.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Q1157 https://www.urionlinejudge.com.br/judge/pt/problems/view/1157
+func Q1157() {
+  var N int
+
+  fmt.Scanf("%d", &N)
+
+  for i := 1; i <= N / 2; i++ {
+    if N%i == 0 {
+      fmt.Printf("%d\n", i)
+    }
+  }
+  
+  fmt.Printf("%d\n", N)
+}
