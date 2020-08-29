90f70365e466b2280a82bb37fdddb567d77164b0
Henrique Miranda <enrque.ns@gmail.com>
2020-08-29 14:27:23 -0300

    :sparkles: feat(URI): add question 1089

---
+++ URI/golang/1089.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// Q1089 https://www.urionlinejudge.com.br/judge/pt/problems/view/1089
+func Q1089() {
+	var N int
+
+	for {
+		fmt.Scanf("%d", &N)
+
+		if N == 0 {
+			break
+		}
+
+		var points []int
+		peaks := 0
+
+		for i := 0; i < N; i++ {
+			var point int
+			fmt.Scanf("%d", &point)
+			points = append(points, point)
+		}
+
+		points = append(points, points[0:2]...)
+		
+		for i := 1; i <= N; i++ {
+			if ((points[i] > points[i - 1]) && (points[i] > points[i + 1])) || ((points[i] < points[i - 1]) && (points[i] < points[i + 1])) {
+				peaks = peaks + 1
+			}
+		}
+		
+		fmt.Printf("%d\n", peaks)
+	}
+}
