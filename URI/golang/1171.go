aaa9bdfa810c5c49b64ff16fa8caa8f790e9079a
Henrique Miranda <enrque.ns@gmail.com>
2020-08-29 17:46:54 -0300

    :sparkles: feat(URI): add question 1171

---
+++ URI/golang/1171.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Q1171 https://www.urionlinejudge.com.br/judge/pt/problems/view/1171
+func Q1171() {
+	var N int
+
+	fmt.Scanf("%d", &N)
+
+	values := make(map[int]int)
+	var keys []int
+
+	for i := 0; i < N; i++ {
+		var value int
+		fmt.Scanf("%d", &value)
+		values[value] = values[value] + 1
+	}
+
+	for k := range values { 
+    keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	for _, key := range keys {
+		fmt.Printf("%d aparece %d vez(es)\n", key, values[key])
+	}
+}
