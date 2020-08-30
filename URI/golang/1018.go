181c59aa57c062116c1acf7f84610005107e5170
Henrique Miranda <enrque.ns@gmail.com>
2020-08-30 11:29:43 -0300

    :sparkles: feat(URI): add question 2415 in go

--- URI/golang/1018.go
+++ URI/golang/1018.go
@@ -4,14 +4,14 @@ import "fmt"
 
 // Q1018 https://www.urionlinejudge.com.br/judge/pt/problems/view/1018
 func Q1018() {
-  var N int
+  var N, amount int
   notes := [7]int{100, 50, 20, 10, 5, 2, 1}
 
   fmt.Scanf("%d", &N)
   fmt.Printf("%d\n", N)
 
   for i := 0; i < 7; i++ {
-    amount := N / notes[i]
+    amount = N / notes[i]
     fmt.Printf("%d nota(s) de R$ %d,00\n", amount, notes[i])
     N = N % notes[i]
   }
---
+++ URI/golang/2415.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Q2415 https://www.urionlinejudge.com.br/judge/pt/problems/view/2415
+func Q2415() {
+	var N, points, last, currentPoints, current int
+
+  fmt.Scanf("%d", &N)
+
+  for i := 0; i < N; i++ {
+		fmt.Scanf("%d", &current)
+		
+		if (last == current) || (i == 0) {
+			currentPoints = currentPoints + 1
+		} else {
+			if currentPoints > points {
+				points = currentPoints
+			}
+
+			currentPoints = 1
+		}
+
+		last = current
+	}
+
+	if currentPoints > points {
+		points = currentPoints
+	}
+	
+	fmt.Printf("%d\n", points)
+}
